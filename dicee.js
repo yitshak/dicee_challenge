3de83cd8caba90c14682b269f77c40167c14d33c
yitshak yarom <yitshaky@openeyes-insurance.com>
2022-01-18 09:08:14 +0200

    add function

--- dicee.js
+++ dicee.js
@@ -6,19 +6,24 @@ function IntToDiceImg(number) {
   return "images/dice"+number+".png"
 }
 
+function ChangeClassSrc(the_class, img) {
+  document.getElementsByClassName(the_class)[0].setAttribute("src",img)
+}
+
 var dice_1 = RollDice()
 var dice_2 = RollDice()
 
 var dice_1_img = IntToDiceImg(dice_1)
 var dice_2_img = IntToDiceImg(dice_2)
 
-document.getElementsByClassName("img1")[0].setAttribute("src",dice_1_img)
-document.getElementsByClassName("img2")[0].setAttribute("src",dice_2_img)
+
+ChangeClassSrc("img1", dice_1_img)
+ChangeClassSrc("img2", dice_2_img)
 
 if (dice_1 > dice_2) {
   win_string = "🚩 Player 1 wins"
 } else if (dice_2 > dice_1) {
-  win_string = "🚩 Player 2 wins"
+  win_string = "Player 2 wins 🚩"
 } else {
   win_string = "Draw!!"
 }
